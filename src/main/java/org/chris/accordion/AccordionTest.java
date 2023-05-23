0b3f9462b89c756ef2b09ac622149a58b54164a9
Ezzerof <christian.bitca@gmail.com>
2023-05-23 17:16:33 +0100

    open and close accordions test

---
+++ src/main/java/org/chris/accordion/AccordionTest.java
@@ -0,0 +1,29 @@
+package org.chris.accordion;
+
+import org.chris.WebdriverConfiguration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AccordionTest {
+
+    static WebDriver driver = WebdriverConfiguration.getChromeDriver();
+    private static String accordionpageURL = "https://automationtesting.co.uk/accordion.html";
+
+    public static void pressOnAllAccordions(int n) {
+        driver.get(accordionpageURL);
+
+        for (int i = 0; i != n; i++) {
+            driver.findElement(By.xpath("//div[normalize-space()='Platform Portability']")).click();
+            driver.findElement(By.xpath("//div[normalize-space()='Language Support']")).click();
+            driver.findElement(By.xpath("//div[normalize-space()='Selenium Grid']")).click();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        driver.close();
+        driver.quit();
+    }
+
+}
---
+++ src/main/java/org/chris/accordion/AccordionTestStarter.java
@@ -0,0 +1,9 @@
+package org.chris.accordion;
+
+public class AccordionTestStarter {
+
+    public static void main(String[] args) {
+        AccordionTest.pressOnAllAccordions(3);
+    }
+
+}
