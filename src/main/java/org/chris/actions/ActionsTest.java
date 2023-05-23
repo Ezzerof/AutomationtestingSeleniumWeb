e306cb9600d28172950c68807b76aee6eb7dfb46
Ezzerof <christian.bitca@gmail.com>
2023-05-23 15:56:24 +0100

    add assertion for Actions page

--- src/main/java/org/chris/actions/ActionsTest.java
+++ src/main/java/org/chris/actions/ActionsTest.java
@@ -8,17 +8,28 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ActionsTest {
     // Not working
-//    private static WebDriver driver = WebdriverConfiguration.getChromeDriver();
-    private static WebDriver driver = WebdriverConfiguration.getMacChromeDriver();
+    private static WebDriver driver = WebdriverConfiguration.getChromeDriver();
+//    private static WebDriver driver = WebdriverConfiguration.getMacChromeDriver();
     private static final String actionsPageURL = "https://automationtesting.co.uk/actions.html";
     private static final By dragMeButton = By.xpath("//p[@id='dragtarget']");
     private static final By placeWhereToDrag = By.cssSelector("(//div[@class='droptarget'])[2]");
 
-    public static void goToTheActionPage() throws InterruptedException {
+    public static void goToTheActionPage() {
+        driver.get(actionsPageURL);
+        String pageName = driver.getTitle();
+        assertEquals("Actions", pageName);
+        driver.close();
+        driver.quit();
+    }
+
+    public static void interactWithDragMeButton() throws InterruptedException {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(actionsPageURL);
+
         WebElement sourceElement = driver.findElement(dragMeButton);
         WebElement targetElement = driver.findElement(placeWhereToDrag);
         Actions action = new Actions(driver);
--- src/main/java/org/chris/actions/ActionsTestStarter.java
+++ src/main/java/org/chris/actions/ActionsTestStarter.java
@@ -3,7 +3,8 @@ package org.chris.actions;
 public class ActionsTestStarter {
 
     public static void main(String[] args) throws InterruptedException {
-//        ActionsTest.goToTheActionPage();
-        ActionsTest.doubleClick();
+        ActionsTest.goToTheActionPage();
+//        ActionsTest.doubleClick();
+
     }
 }
