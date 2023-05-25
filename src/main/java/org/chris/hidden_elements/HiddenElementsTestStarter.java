b1095c44391274ee634df091d4045d658ec32c81
Ezzerof <christian.bitca@gmail.com>
2023-05-25 14:54:42 +0100

    hidden elements page test

---
+++ src/main/java/org/chris/hidden_elements/HiddenElementsTestStarter.java
@@ -0,0 +1,8 @@
+package org.chris.hidden_elements;
+
+public class HiddenElementsTestStarter {
+
+    public static void main(String[] args) {
+        HiddenElementsTest.showHiddenElements();
+    }
+}
