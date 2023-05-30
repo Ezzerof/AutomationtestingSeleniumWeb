7577ec4396b3f1ada0fd51b391904d9b20394610
Ezzerof <christian.bitca@gmail.com>
2023-05-30 17:00:45 +0100

    selecting origin and destination on easyjet website

---
+++ src/main/java/org/chris/easyjet/EasyJetTest.java
@@ -0,0 +1,60 @@
+package org.chris.easyjet;
+
+import org.chris.WebdriverConfiguration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class EasyJetTest {
+    static WebDriver driver = WebdriverConfiguration.getChromeDriver();
+    //    static WebDriver driver = WebdriverConfiguration.getEdgeDriver() ;
+    private static String easyJetURL = "https://www.easyjet.com/en/";
+
+    public static void goToEasyJet() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(easyJetURL);
+
+        // closing Cookie popup
+        driver.findElement(By.cssSelector("#ensCloseBanner")).click();
+
+        // entering Origin
+        driver.findElement(By.cssSelector("input[name='origin']")).click();
+        driver.findElement(By.cssSelector("input[name='origin']")).sendKeys("London");
+
+        List<WebElement> origins = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#ui-id-1 >li>a>span")));
+
+        // Selecting Luton from all available airports
+        for (WebElement origin : origins) {
+            if (origin.getText().contains("Luton")) {
+                origin.click();
+            }
+        }
+
+
+        // entering destination
+        driver.findElement(By.cssSelector("input[name='destination']")).click();
+        driver.findElement(By.cssSelector("input[name='destination']")).sendKeys("Paris");
+        List<WebElement> destinations = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("#ui-id-2 >li>a>span")));
+
+        // Selecting Orly from all Paris airports
+        for (WebElement destination : destinations) {
+            if (destination.getText().contains("Orly")) {
+                destination.click();
+            }
+        }
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.close();
+        driver.quit();
+    }
+
+}
---
+++ src/main/java/org/chris/easyjet/EasyJetTestStarter.java
@@ -0,0 +1,7 @@
+package org.chris.easyjet;
+
+public class EasyJetTestStarter {
+    public static void main(String[] args) {
+        EasyJetTest.goToEasyJet();
+    }
+}
